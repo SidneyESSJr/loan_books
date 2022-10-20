25552ec4d400de543c78be5f44e2ceefa7490da5
Sidney Junior <sidney.e.s.s.jr@gmail.com>
2022-10-20 19:24:04 -0300

    create the project structure

---
+++ backend/src/test/java/br/com/loan_books/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.loan_books.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
